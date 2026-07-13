e8049f910416623a2a7208a5de62d083e114bb6c
d4rkn3mo <f07187681@gmail.com>
2026-07-13 17:10:14 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+int main()
+{
+    int key;
+    std::cout << "Enter a key( integer 1-255 ): ";
+    std::cin >> key;
+    std::cin.ignore();
+
+    std::string payload;
+    std::cout << "Enter text to obfuscate: ";
+    std::getline(std::cin, payload);
+
+    std::cout << "\nObfuscated byte array: " << std::endl;
+    std::cout << "{ ";
+
+    for (size_t i =0; i < payload.length(); i++)
+    {
+        char current_char = payload[i];
+
+        char obfuscated_char = current_char ^ key;
+
+        std::cout << "0x" << std::hex << (int)(unsigned char)obfuscated_char;
+
+        if (i < payload.length() - 1)
+        {
+            std::cout << ", ";
+        }
+
+
+    }
+
+    std::cout << " };" << std::endl;
+    std::cout << "\n\nDone." <<std::endl;
+    return 0;
+}
